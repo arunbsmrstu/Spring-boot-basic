920f1d6d88fbeb2410aec0b5c8fbebe0ee16ea81
arun <arunbsmrstu@gmail.com>
2019-09-16 17:56:38 +0600

    initialization

---
+++ src/main/java/com/example/demotest/DemotestApplication.java
@@ -0,0 +1,29 @@
+package com.example.demotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@RestController
+public class DemotestApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemotestApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotestApplication.class, args);
+    }
+
+//    @RequestMapping(value = "/")
+//    public String hello() {
+//        return "test data file";
+//    }
+
+}
